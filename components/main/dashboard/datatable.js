974cc0f918dfbee2636ef122550c86ccd860055f
Pyrus-vagus <grushadaria@gmail.com>
2021-07-30 19:44:42 +0300

    apply styles to dashboard

--- components/main/dashboard/datatable.js
+++ components/main/dashboard/datatable.js
@@ -3,14 +3,15 @@ import { movieListID } from "../../variables.js";
 export const movieList = {
   view: "datatable",
   columns:[
-    {id: "rank", header: [{text: "", rowspan: 2}],  width:50},
+    {id: "rank", header: [{text: "", rowspan: 2}], css:"rank",  width:50},
     {id: "title", header: ["Film Title", {content: "textFilter"}], width: 200, fillspace: true, sort:"string"},
     {id: "year", header: ["Realesed", {content: "textFilter"}], sort:"string"},
     {id: "votes", header: ["Votes", {content: "textFilter"}], sort:"string"},
     {id: "rating", header:["Rating", {content: "textFilter"}], sort:"string"},
-    {template: "<span class='webix_icon wxi-trash'></span>"}
-  ],
+    {template: "<span class='webix_icon wxi-trash'></span>"},
+    ],
   url: "components/main/dashboard/data/data.js",
+  hover: "myhover",
   gravity: 2.5,
   scrollX: false,
   select: true,
